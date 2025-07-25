1d082b7933d57d8c4f560904ca18c1f07e07e510
aliimtiaz <aliimtiaz8282@gmail.com>
2025-07-25 15:12:35 +0500

    404 page added

---
+++ src/app/not-found.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const notfound = () => {
+  return (
+    <div className='w-full text-4xl text-center mt-30 justify-center flex'>
+        <div className='border w-1/2 p-5 rounded-lg text-indigo-400 shadow-2xl shadow-indigo-300'>
+                <h1>404 Error Page not found</h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default notfound
